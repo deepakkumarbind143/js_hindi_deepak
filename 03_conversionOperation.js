29cee93a684b0cb7a9fddd9f1e59807db753bd04
deepakkumarbind143 <deepakkumarbind143>
2026-01-28 09:45:54 +0530

    converstion

---
+++ 03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score="deepak"
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" =>33
+//"33an"=>NaN
+//true =>1; false=>0
+
+let isLoggedIn="d"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=>false
+//"" =>false
+// "Deepak" =>true
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber);
