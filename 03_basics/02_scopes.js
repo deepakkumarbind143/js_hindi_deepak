602a9813264dd04903ed1f93cdea949efc8aae80
deepakkumarbind143 <deepakkumarbind949@gmail.com>
2026-01-30 10:21:53 +0530

    fuction scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,6 +2,42 @@ let a = 10
 const b = 20
 var c = 30
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+//DOM  => Document object model
+
+
+//* Nested scope
+
+function one(){
+    const username = "deepak"
+
+    function two(){
+        const website = "youtube"
+console.log(username);
+
+        
+    }
+    // console.log(website);
+    two()//if it not called than the function one will never executed
+
+}
+
+// one()
+
+//++++++++++++++++++++++++++++++++interesting++++++++++++++++++++++++++++++
+
+function addone(num){//it is function
+    return num + 1
+}
+
+addone(5)
+
+const addTwo = function(num){//it is function also called as expression
+    return num + 2
+}
+
+addTwo(5)
