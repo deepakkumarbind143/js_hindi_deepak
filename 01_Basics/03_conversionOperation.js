cd9f24472e6653c8afa44057e7c28fad4c2c0f84
deepakkumarbind143 <deepakkumarbind949@gmail.com>
2026-01-30 19:17:58 +0530

    revised

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -7,9 +7,9 @@ let valueInNumber= Number(score)
 // console.log(typeof valueInNumber)
 // console.log(valueInNumber)
 
-//"33" =>33
-//"33an"=>NaN
-//true =>1; false=>0
+//"33" =>33    // we convert this in number
+//"33an"=>NaN  // we canont convert this in number
+//true =>1; false=>0  
 
 let isLoggedIn="d"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
@@ -38,27 +38,30 @@ let str3 = str1 + str2
 // console.log(str3);
 
 
-// console.log("1" + 1);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 1);        // 11
+// console.log(1 + "2");     //12
+// console.log("1" + 2 + 2);  //122
+// console.log(1 + 2 + "2"); //32
 
 // console.log((3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); //1 //if we add + then we get output in number or else output is true
+// console.log(+false);//0
+// console.log(+"");//0
 
 
 // x is 4; y is 3
 let x = 3;
-const y = x++;
-console.log(`x is ${x} , y is ${y}`);
+const y = x++; // here x will be 3 and return 4 to x variable
+// console.log(`x is ${x} , y is ${y}`);
 
 
 // x2 is 4n; y2 is 3n
 let x2 = 3n;
+// console.log(typeof x2);// Bingint adding n to last of the assign number
+
 const y2 = x2++;
-console.log(`x is ${x} , y is ${y}`);
+// console.log(`x is ${x} , y is ${y}`);
 
 
 
--- 01_Basics/04_comparisons.js
+++ 01_Basics/04_comparisons.js
@@ -1,15 +1,17 @@
-// console.log("2" > 1);
-// console.log("02" > 1);
-
-console.log(null > 0);
-console.log(null > 0);
-console.log(null >= 0);
+// console.log("2" > 1);// "2"=>2
+// console.log("02" > 1); // 0 does not matther "02"=> 2
 
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+
+// console.log(null > 0); //null =>0
+// console.log(null > 0);// null =>0
+// console.log(null >= 0); // comparison operater converts null => 0 so output will be true
+
+
+// console.log(undefined == 0); //false
+// console.log(undefined > 0);//false
+// console.log(undefined < 0);//false
 
 // ===
 
-console.log("2" === 0);
+// console.log("2" === 0);//false
