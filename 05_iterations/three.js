5e0f45c73b619e48ee0089b06ef1d2cefd0a8f8a
deepakkumarbind143 <deepakkumarbind949@gmail.com>
2026-01-31 11:45:01 +0530

    for of

---
+++ 05_iterations/three.js
@@ -0,0 +1,40 @@
+//for of 
+
+// ["", "", ""]//list of string
+// [{}, {}, {}]//list of object
+
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const element of arr) {
+    // console.log(` this is the elements of list ${element}`);
+    
+}
+
+// Maps
+
+const map = new Map()// map know for unics
+map.set('IN', "INDIA")
+map.set('IN', "INDIA")//this will not count and come as output
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+
+// console.log(map);
+
+
+//intresting [] use this and assign key and value to print any
+for (const [key, value] of map) {
+    // console.log(key);
+    // console.log(key, `:-`, value);//this one print both
+    
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {  // this for loop not work on objeect
+//     console.log(key);
+    
+// }
