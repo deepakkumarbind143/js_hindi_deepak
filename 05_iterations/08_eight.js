158b32e10272ed6653d9d1c870e7662a6732a127
deepakkumarbind143 <deepakkumarbind949@gmail.com>
2026-01-31 19:16:47 +0530

    reduce

---
+++ 05_iterations/08_eight.js
@@ -0,0 +1,35 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval){
+//     console.log(`acc: ${acc} and the currval: ${currval}`);
+    
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce((acc, curr)=> acc + curr, 0)
+console.log(myTotal);
+
+
+
+//performing reduce iteration on object
+// const shopping = [
+//     {
+//         itemnames: "js cource",
+//         price: 290,
+//     },
+//     {
+//         itemnames: "python",
+//         price: 290,
+//     },
+//     {
+//         itemnames: "mobile dev",
+//         price: 290,
+//     },
+//     {
+//         itemnames: "data scientist",
+//         price: 290,
+//     },
+// ]
+
+//  const priceToPay = shopping.reduce((acc, item)=> acc + item.price, 0 )
+// console.log(priceToPay);
