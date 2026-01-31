03f7534fb72bdb0d4199bb1bffe70b58b3175c32
deepakkumarbind143 <deepakkumarbind949@gmail.com>
2026-01-31 10:27:29 +0530

    string revisions

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -8,32 +8,32 @@ console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 const gameName = new String(`deepak-bind`)
 //when you used new key word you used object
 
-// console.log(gameName[0]);
+// console.log(gameName[0]);//d
 // console.log(gameName.__proto__);
 
 
-// console.log(gameName.length);
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('p'));
-console.log(gameName.length);
+console.log(gameName.length);    //11 object always counts keys, so its start from 1
+console.log(gameName.charAt(2));//e
+console.log(gameName.indexOf('p'));//3
+
+console.log( typeof gameName);//object
 
 
 
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
-
-const anotherString = gameName.slice(-8, 5)
-console.log(anotherString);
-
-
-const newStringOne = "   deepak  "
-console.log(newStringOne.trim());
+// const anotherString = gameName.slice(-8, 5)
+// console.log(anotherString);
 
 
-const url ="https://deepak.com/deepak%20bind"
+// const newStringOne = "   deepak  "
+// console.log(newStringOne.trim());
 
-console.log(url.replace('%20','-'));
 
-console.log(url.includes('deepak'))
-console.log(gameName.split('-'));
+// const url ="https://deepak.com/deepak%20bind"
+
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('deepak'))
+// console.log(gameName.split('-'));
