74065ba43f7efefdc8be58d2b7a0f23a57552500
deepakkumarbind143 <deepakkumarbind949@gmail.com>
2026-01-31 17:09:15 +0530

    for each

---
+++ 05_iterations/05_five.js
@@ -0,0 +1,55 @@
+const coding = ["js", "ruby", "java", "python", "dart"]
+
+// coding.forEach( function(item){  // normal function
+//     console.log(item);
+    
+// })
+
+
+
+// coding.forEach(  (item)=>{ //arrow function
+// console.log(item);
+
+// })
+
+
+//calling function with reference
+
+// function printMe(item) {
+// console.log(item);
+
+// }
+// coding.forEach(printMe)
+
+
+// for each always have value, index, array
+// coding.forEach(  (item, index, arr)=>{
+//     console.log(item, index, arr);
+    
+// })
+
+
+// here you get object value that is in list
+// const mycoding = [
+//     {
+//         languageName: "javascripts",
+//         languageFileName: "js"
+//     },
+//     {
+//         languageName: "java",
+//         languageFileName: "java"
+//     },
+//     {
+//         languageName: "python",
+//         languageFileName: "py"
+//     },
+//     {
+//         languageName: "c++",
+//         languageFileName: "cpp"
+//     }
+// ]
+
+// mycoding.forEach( (item)=>{
+// console.log(item.languageName);
+
+// })
