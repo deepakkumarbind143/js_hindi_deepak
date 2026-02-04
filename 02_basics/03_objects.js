930a192976bc625d15c306773972830427ca0da4
deepakkumarbind143 <deepakkumarbind949@gmail.com>
2026-02-04 13:48:51 +0530

    object revision

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -39,3 +39,68 @@ console.log(JsUser.greeting());//this will give output of function ,and if you a
 console.log(JsUser.greeting2());
 
 
+
+
+//
+
+
+// A. Accessing Data
+// There are two ways to get data out of an object. You must know both:
+
+// Dot Notation: intern.firstName (Used 90% of the time).
+
+// Bracket Notation: intern["firstName"] (Used when the property name is stored in a variable).
+
+const myage = "age";
+
+const intern = {
+  
+  firstName: "deepak",
+  age: 21,
+  skills: ["javascript", "html"],
+  isReady: true,
+  // a function inside object is called method
+  greet: function(){
+     return "hello, i am ready to code";
+  }
+};
+
+console.log("-----------DESTRUCTURING---------")
+
+const {firstName, age, skills, isReady, greet} = intern
+
+console.log(firstName)
+console.log(age)
+console.log(skills)
+console.log(isReady)
+console.log(greet())
+
+console.log("-----------UPDATING AND ADDING---------")
+
+intern.skills[2] = "dart"
+intern.isReady = false
+intern.greet = function(){
+  return "hello its an update"
+}
+console.log("-----------PRINITING ELEMENT OF ARRAY---------")
+
+ intern.skills.forEach((str)=> console.log(str))
+ let allarray = intern.skills.map((str)=> str)
+console.log(allarray)
+console.log(intern.isReady)
+console.log(intern.greet())
+
+console.log("-----------SPREAD OPERATORS---------")
+
+
+const updatedinter = {
+  ...intern,
+  isReady :true,
+  firstName: "noname"
+}
+
+ const { isReady : ready, firstName : name} = updatedinter;
+console.log(ready)
+console.log(name)
+// Spread (...) creates a new object.
+// The old object (intern) stays exactly the same.
